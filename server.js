208ab22e7aa0c3c4cb720ff0cc8a65d90147578e
Akshay Jadhav <akshayjadhav930@gmail.com>
2022-06-02 08:45:44 +0530

    changes

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/scalio-angular'));
+app.use(express.static(__dirname + '/dist/scalio'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/scalio-angular/index.html'));
+res.sendFile(path.join(__dirname+'/dist/scalio/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
